793f5a87c1625ab7a8222e31067ef91ed7dff4a9
maria-magdalena-bogoeva <maggiiiiii@abv.bg>
2023-03-22 12:28:49 +0200

    contacts and register

--- BulgariaApp/BulgariaApp/Controllers/HomeController.cs
+++ BulgariaApp/BulgariaApp/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace BulgariaApp.Controllers
         {
             return View();
         }
-
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
